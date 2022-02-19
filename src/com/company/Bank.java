10cd4ec94cf5348c036a5924de305fb4dc3b3243
MikeMwai <mwai.michael@strathmore.edu>
2022-02-19 21:35:07 +0300

    New Files

---
+++ src/com/company/Bank.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Bank
+{
+    public void account_creation()
+    {
+        System.out.println("A new account has been created!");
+    }
+    public void deposits()
+    {
+        System.out.println("A new deposit has been made...");
+    }
+    public void withdrawals()
+    {
+        System.out.println("A new withdrawal has been made...");
+    }
+    public void status_check()
+    {
+        System.out.println("Status check ongoing...");
+    }
+}
